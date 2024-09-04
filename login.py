e95266d6ce9e9b2e8ece0f508c85f5dbd9f7ff81
Camilov1814 <camilo.valencia58@eia.edu.co>
2024-09-03 22:57:21 -0500

    feat: Add Pronosticos page to the menu

--- login.py
+++ login.py
@@ -18,8 +18,9 @@ def generarMenu(usuario):
         st.page_link('inicio.py',label='Inicio', icon=':material/home:')
         st.subheader('Tableros')
         st.page_link('pages/Analisis_de_costos.py',label='Analisis de costos', icon=':material/attach_money:')
-        st.page_link('pages/Analisis_de_escenarios.py',label='Analisis de escenarios', icon=':material/monetization_on:')
+        st.page_link('pages/Analisis_de_escenarios.py',label='Analisis de escenarios', icon=':material/emergency:')
         st.page_link('pages/Banco_de_datos.py',label='Banco de datos', icon=':material/account_balance:')
+        st.page_link('pages/Pronosticos.py',label='Pronosticos', icon=':material/online_prediction:')
         btnSalir = st.button('Salir')
         if btnSalir:
             st.session_state.clear()    
