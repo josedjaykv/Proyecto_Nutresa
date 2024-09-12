8376127c9603754b9767b4658fd2c9da62f56417
unknown <sacosa634@gmail.com>
2024-09-12 14:03:25 -0500

    Avance pagina pronosticos

--- pages/Analisis_de_escenarios.py
+++ pages/Analisis_de_escenarios.py
@@ -6,7 +6,7 @@ import pandas as pd
 login.generar_login()
 if 'usuario' in st.session_state:
     st.header('Analisis de :orange[escenarios]')
-    
+
     st.write("Escenarios de costos de diferentes módulos bajo diferentes trayectorias climáticas")
 
     #datos
@@ -16,81 +16,81 @@ if 'usuario' in st.session_state:
     Disel_precio = pd.read_csv('NewData/Disel_precio.csv')
     Energia_termica = pd.read_csv('NewData/Energia_termica.csv')
     Emisiones = pd.read_csv('NewData/Emisiones.csv')
-    
+
 
     st.subheader("Consumo de :orange[Energía Promedio Mensual]")
     st.line_chart(Promedio_energia.set_index('Mes'))
-    
-    
+
+
     precio_energia['Date'] = pd.to_datetime(precio_energia['Date'], dayfirst=True)
     precio_energia = precio_energia.sort_values(by='Date')
-    
+
     st.subheader("Precio de la :orange[Energía]")
     st.line_chart(precio_energia.set_index('Date'))
-    
+
     st.subheader("Combustible flota :orange['Disel']")
     st.line_chart(Disel_precio.set_index('Year-Month'))
 
     st.subheader("Energía térmica :orange[Nutresa]")
     st.line_chart(Energia_termica.set_index('Date'))
-    
+
     st.subheader("Emisiones de :orange[CO2]")
     st.line_chart(Emisiones.set_index('Date'))
-    
+
     st.subheader("Costo de :orange[Materia Prima]")
-    
+
     options = {
-    "title": {"text": ""},
-    "tooltip": {
-        "trigger": "axis",
-        "axisPointer": {"type": "cross", "label": {"backgroundColor": "#6a7985"}},
-    },
-    "legend": {"data": ['Precio_cacao_und', 'Precio_cafe', 'Precio_carne_cerdo', 'Precio_carne_res']},
-    "toolbox": {"feature": {"saveAsImage": {}}},
-    "grid": {"left": "3%", "right": "3%", "bottom": "3%", "containLabel": True},
-    "xAxis": [
-        {
-            "type": "category",
-            "boundaryGap": False,
-            "data":  Materia_prima['Date'].tolist(),
-        }
-    ],
-    "yAxis": [{"type": "value"}],
-    "series": [
-        {
-            "name": 'Precio_cacao_und',
-            "type": "line",
-            "stack": "total",
-            "areaStyle": {},
-            "emphasis": {"focus": "series"},
-            "data": Materia_prima['Precio_cacao_und'].tolist(),
+        "title": {"text": ""},
+        "tooltip": {
+            "trigger": "axis",
+            "axisPointer": {"type": "cross", "label": {"backgroundColor": "#6a7985"}},
         },
-        {
-            "name": 'Precio_cafe',
-            "type": "line",
-            "stack": "total",
-            "areaStyle": {},
-            "emphasis": {"focus": "series"},
-            "data": Materia_prima['Precio_cafe'].tolist(),
-        },
-        {
-            "name": 'Precio_carne_cerdo',
-            "type": "line",
-            "stack": "total",
-            "areaStyle": {},
-            "emphasis": {"focus": "series"},
-            "data": Materia_prima['Precio_carne_cerdo'].tolist(),
-        },
-        {
-            "name": 'Precio_carne_res',
-            "type": "line",
-            "stack": "total",
-            "areaStyle": {},
-            "emphasis": {"focus": "series"},
-            "data": Materia_prima['Precio_carne_res'].tolist(),
-        },
-    ],
-}
+        "legend": {"data": ['Precio_cacao_und', 'Precio_cafe', 'Precio_carne_cerdo', 'Precio_carne_res']},
+        "toolbox": {"feature": {"saveAsImage": {}}},
+        "grid": {"left": "3%", "right": "3%", "bottom": "3%", "containLabel": True},
+        "xAxis": [
+            {
+                "type": "category",
+                "boundaryGap": False,
+                "data":  Materia_prima['Date'].tolist(),
+            }
+        ],
+        "yAxis": [{"type": "value"}],
+        "series": [
+            {
+                "name": 'Precio_cacao_und',
+                "type": "line",
+                "stack": "total",
+                "areaStyle": {},
+                "emphasis": {"focus": "series"},
+                "data": Materia_prima['Precio_cacao_und'].tolist(),
+            },
+            {
+                "name": 'Precio_cafe',
+                "type": "line",
+                "stack": "total",
+                "areaStyle": {},
+                "emphasis": {"focus": "series"},
+                "data": Materia_prima['Precio_cafe'].tolist(),
+            },
+            {
+                "name": 'Precio_carne_cerdo',
+                "type": "line",
+                "stack": "total",
+                "areaStyle": {},
+                "emphasis": {"focus": "series"},
+                "data": Materia_prima['Precio_carne_cerdo'].tolist(),
+            },
+            {
+                "name": 'Precio_carne_res',
+                "type": "line",
+                "stack": "total",
+                "areaStyle": {},
+                "emphasis": {"focus": "series"},
+                "data": Materia_prima['Precio_carne_res'].tolist(),
+            },
+        ],
+    }
 
 # Mostrar el gráfico en Streamlit
 st_echarts(options=options, height="400px", width="100%")
--- pages/Pronosticos.py
+++ pages/Pronosticos.py
@@ -1,6 +1,38 @@
 import streamlit as st
 import login
+import os
+import csv
+import pandas as pd
 
 login.generar_login()
+
 if 'usuario' in st.session_state:
     st.header(':orange[Pronosticos]')
+
+    UPLOAD_FOLDER = 'uploads'
+    if not os.path.exists(UPLOAD_FOLDER):
+        os.makedirs(UPLOAD_FOLDER)
+
+    if os.listdir(UPLOAD_FOLDER):
+        files = list()
+        st.write("Archivos guardados:")
+        for file_name in os.listdir(UPLOAD_FOLDER):
+            if file_name.endswith(".csv"):
+                files.append(file_name)
+
+    st.write(files)
+
+    def get_delimiter(file_path, bytes = 4096):
+        with open(file_path, "r").read(bytes) as data
+        sniffer = csv.Sniffer()
+        delimiter = sniffer.sniff(data).delimiter
+        return delimiter
+
+    archivo_selec = st.selectbox("Seleccionar archivo", files)
+    path_archivo = os.path.join(f'uploads/{archivo_selec}')
+    archivo = pd.read_csv(path_archivo, delimiter=get_delimiter(path_archivo, 4096))
+
+    archivo
+
+    st.subheader("Combustible flota :orange['Disel']")
+    st.line_chart(archivo.set_index('Year-Month'))
