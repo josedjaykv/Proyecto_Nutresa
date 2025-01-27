96c689393bfa3bda6182226c063886f892a81d9a
josedjaykv <josedjaykv@gmail.com>
2025-01-27 15:48:25 -0500

    Seleccionar etiquetas mejorado

--- pages/Banco_de_datos.py
+++ pages/Banco_de_datos.py
@@ -25,32 +25,31 @@ if 'usuario' in st.session_state:
         'Sin etiqueta': 'gray'
     }
 
+
+    # Funciones
+    ##################################################################################
+
     # Función para redirigir
     def redirect_to_showfile(file_name):
         st.session_state['file'] = file_name
         st.session_state['user'] = st.session_state['usuario'] # Para mantener en sesión al usuario
         st.switch_page('pages/showfiles.py')
 
-    # Función para guardar archivo y etiqueta en la base de datos
-    def save_to_db(file_path, label):
-        conn = sqlite3.connect("database/archivos.db")
-        cursor = conn.cursor()
 
-        # Insertar archivo y etiqueta
-        cursor.execute("INSERT INTO archivos (direccion, etiqueta) VALUES (?, ?)", (file_path, label))
-        conn.commit()
-        conn.close()
+    # Lógica
+    ##################################################################################
 
-    # Cargar múltiples archivos
+    # Se cargan multiples archivos
     uploaded_files = st.file_uploader(
         "Cargar Archivos CSV o Excel", accept_multiple_files=True, type=['csv', 'xlsx']
     )
 
     #######################
-    # Mostrar un "pop-up" de selección de etiqueta al cargar un archivo
+    # Se hace la copia de los archivos luego de cargarlos
     if uploaded_files:
         for uploaded_file in uploaded_files:
             file_path = os.path.join(UPLOAD_FOLDER, uploaded_file.name)
+            print(file_path)
             with open(file_path, "wb") as f:
                 f.write(uploaded_file.getbuffer())
 
@@ -62,27 +61,14 @@ if 'usuario' in st.session_state:
             st.session_state['show_label_select'] = True
 
     ###################### Mejorar esta parte para que se vea mejor
-    # Si hay un archivo cargado y si se debe seleccionar una etiqueta
+    # Luego de que se carga el archivo se selecciona la etiqueta del archivo
     if 'show_label_select' in st.session_state and st.session_state['show_label_select']:
-        st.write(f"Seleccionar etiqueta de riesgo para el archivo: {st.session_state['selected_file']}")
-        label = st.selectbox("Etiqueta de riesgo:", 
-                             ['Riesgo regulatorio', 'Riesgo tecnológico', 'Riesgo de mercado'], key='label_select')
+        st.switch_page('pages/Seleccionar_etiquetas.py')
 
-        # Cuando se confirma la etiqueta, desaparecer el selector
-        if st.button('Confirmar etiqueta'):
-            # Guardar en la base de datos
-            save_to_db(st.session_state['selected_file_path'], label)
-
-            st.success(f"Archivo guardado: {st.session_state['selected_file']} - "
-                       f"Etiqueta '{label}' asignada al archivo.")
-
-            # Hacer que desaparezca el selector de etiqueta
-            st.session_state['show_label_select'] = False
 
     # Mostrar archivos guardados con sus etiquetas y colores desde la base de datos
     conn = sqlite3.connect("database/archivos.db")
     cursor = conn.cursor()
-
     cursor.execute("SELECT direccion, etiqueta FROM archivos")
     archivos = cursor.fetchall()
     conn.close()
@@ -93,8 +79,13 @@ if 'usuario' in st.session_state:
             file_name = os.path.basename(file_path)
             color = risk_colors.get(label, 'gray')
 
-            # Mostrar el archivo con su etiqueta coloreada
-            st.markdown(f"<span style='color:{color}; font-weight:bold;'>{label}</span> - {file_name}",
-                        unsafe_allow_html=True)
-            if st.button(f"Ver {file_name}"):
-                redirect_to_showfile(file_name)
+            col1, col2, col3 = st.columns(3)
+            with col1:
+                if st.button(f"Ver {file_name}", key=f"Ver_{file_name}"):
+                    redirect_to_showfile(file_name)
+            with col2:
+                st.markdown(f"<span style='color:{color}; font-weight:bold;'>{label}</span>",
+                            unsafe_allow_html=True)
+                
+
+
